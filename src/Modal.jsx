1afcbb15041439bd2729c6f99cf67ce76e267953
JonasPaturej <89771974+JonasPaturej@users.noreply.github.com>
2026-06-19 15:56:12 +0200

    Suppression des App, création de la fonction modal avec son css et index.js pour exporter

---
+++ src/Modal.jsx
@@ -0,0 +1,19 @@
+import "./Modal.css";
+
+function Modal({ isOpen, onClose, children }) {
+  if (!isOpen) return null;
+
+  return (
+    <div className="modal-overlay">
+      <div className="modal-content">
+        <button type="button" onClick={onClose}>
+          X
+        </button>
+        
+        {children}
+      </div>
+    </div>
+  );
+}
+
+export default Modal;
