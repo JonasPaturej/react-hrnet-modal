0483bda043cba9cfd649d57609fce57e68feba47
JonasPaturej <89771974+JonasPaturej@users.noreply.github.com>
2026-06-22 12:09:18 +0200

    modification de vite.config.js et de package.json, ajout du dossier dist grace à npm run build

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { resolve } from "path";
+import { fileURLToPath } from "url";
 
-// https://vite.dev/config/
+const __dirname = fileURLToPath(new URL(".", import.meta.url));
+
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    lib: {
+      entry: resolve(__dirname, "src/index.js"),
+      name: "ReactHrnetModal",
+      fileName: "react-hrnet-modal",
+    },
+    rollupOptions: {
+      external: ["react", "react-dom"],
+    },
+  },
+});
